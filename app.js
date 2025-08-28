5ffe92fe8b63df981c33155ba4839ca6ed16ce87
mishraabhi09 <abhiimishra1502@gmail.com>
2025-08-28 17:31:31 +0530

    route Added

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
+const cookieparser = require("cookie-parser");
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
+app.set("view engine","ejs");
+
+
+app.get("/login" , function(req , res)
+{
+    res.send("hey this is login route!!")
+})
 
 app.get("/", (req, res) => {
     res.send("chal raha hai!!");
