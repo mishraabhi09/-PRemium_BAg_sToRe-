9188ef96156c4c990b48b283e7b14a2b44d37c3a
mishraabhi09 <abhiimishra1502@gmail.com>
2025-09-06 15:51:14 +0530

    onwer creation Added

--- config/mongoose_connection.js
+++ config/mongoose_connection.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
+const debug = require("debug")("development:mongoose");
+const config = require("config");
+
+// $env:DEBUG="development:*"; node app.js -->> this is the way to set up in the terminal 
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/PRemium_BAg_stoRE")
+    .connect(`${config.get
+        ("MONGO_URI")}/
+         PRemium_BAg_stoRE}`)
     .then(() => {
-        console.log("Db is connected!!");
+        // console.log("Db is connected!!");
+        debug("Server started...")
     })
     .catch((err) => {
-        console.log(err.message);
+        // console.log(err.message);
+        debug(err.message);
     });
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/ownerModel.js");
 
-router.get("/" , (req, res)=>{
+router.get("/", (req, res) => {
     res.send("Hey , this is the owner router route")
 });
+
+
+if (process.env.NODE_ENV = "development") {
+    router.post("/create", async (req, res) => {
+        let owners = await ownerModel.find();
+        if (owners.length > 0) {
+            return res
+                .status(503)
+                .send("You do not allow to create an owner")
+        }
+
+        let { fullname, email, password } = req.body;
+
+        let createdUser = await ownerModel.create({
+            fullname,
+            email,
+            password,
+
+        })
+        res.status(201).send(createdUser);
+        // console.log(createdUser)
+    })
+}
+
+
+
 
 module.exports = router;
